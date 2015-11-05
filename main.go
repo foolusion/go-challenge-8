dab67966edeef509bc1bd78cea598271d16f7ca0
Andrew O'Neill <foolusion@gmail.com>
2015-11-05 11:04:18 -0500

    going to try Peter Norvig's problem structure
    Peter Norvig wrote a sudoku solver in python that works incredibly well.
    I am going to try to implement his problem structure in my go program.
    It should significantly reduce the time it takes to check squares since
    all the possible values will be held for each state.

--- main.go
+++ main.go
@@ -32,17 +32,74 @@ func boardstring(s state, e error) string {
 	return fmt.Sprintf("%v\n%v\n%v\n%v\n%v\n%v\n%v\n%v\n%v\n", string(s[0:9]), string(s[9:18]), string(s[18:27]), string(s[27:36]), string(s[36:45]), string(s[45:54]), string(s[54:63]), string(s[63:72]), string(s[72:81]))
 }
 
+func cross(a, b []string) []string {
+	res := make([]string, len(a)*len(b))
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(b); j++ {
+			res[i*len(b)+j] = a[i] + b[j]
+		}
+	}
+	return res
+}
+
+var (
+	digits  = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	rows    = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
+	cols    = digits
+	squares = cross(rows, cols)
+)
+
+func makeUnitList() [][]string {
+	var unitlist = make([][]string, 0, 27)
+	for c := range cols {
+		unitlist = append(unitlist, cross(rows, cols[c:c+1]))
+	}
+	for r := range rows {
+		unitlist = append(unitlist, cross(rows[r:r+1], cols))
+	}
+	for _, rs := range [][]string{rows[:3], rows[3:6], rows[6:9]} {
+		for _, cs := range [][]string{digits[:3], digits[3:6], digits[6:9]} {
+			unitlist = append(unitlist, cross(rs, cs))
+		}
+	}
+	return unitlist
+}
+
 func main() {
 	defer profile.Start(profile.CPUProfile).Stop()
-	a := problem(
-		"___3_4___" +
-			"_125_834_" +
-			"543671982" +
-			"327156498" +
-			"158942637" +
-			"469783521" +
-			"691835274" +
-			"275419863" +
-			"83426715_")
-	fmt.Println(boardstring(depthLimitedSearch(a, 81)))
+
+	unitlist := makeUnitList()
+
+	fmt.Println(unitlist)
+
+	units := make(map[string][]string, len(squares)*10)
+
+	for _, s := range squares {
+		for _, unit := range unitlist {
+			for _, usq := range unit {
+				if s == usq {
+					units[s] = append(units[s], unit...)
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println(units)
+
+	// TODO peers
+
+	/*
+		a := problem(
+			"___3_4___" +
+				"_125_834_" +
+				"543671982" +
+				"327156498" +
+				"158942637" +
+				"469783521" +
+				"691835274" +
+				"275419863" +
+				"83426715_")
+		fmt.Println(boardstring(depthLimitedSearch(a, 81)))
+	*/
 }
