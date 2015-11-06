b798028dde39bede3e14b68631c8bc71c43a4a96
Andrew O'Neill <foolusion@gmail.com>
2015-11-06 12:54:03 -0500

    more constraint propagation and state implemented

--- grid.go
+++ grid.go
@@ -1,34 +1,75 @@
 package main
 
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	ErrRemovedLastValue = errors.New("Removed last value")
+)
+
+type nstate map[string]string
+
+func newStateFrom(s nstate) nstate {
+	newState := make(nstate, 81*2)
+	for k, v := range s {
+		newState[k] = v
+	}
+	return newState
+}
+
 func parseGrid(grid string) {
 	values := make(map[string][]string, 81*2)
 	for _, s := range squares {
 		values[s] = digits
 	}
 	for s, d := range gridValues(grid) {
-		// FIXME: this is not correct
+		// TODO: real implementation
 		if s == d {
 			return
 		}
 	}
 }
 
-func gridValues(grid string) map[string]string {
-	results := make(map[string]string, 10)
+func gridValues(grid string) nstate {
+	results := make(nstate, 81*2)
 	return results
 }
 
-func assign(values []string, s, d string) bool {
-	// FIXME: need to fix. this does not copy the underlying array
-	other := values
-	for _, d2 := range other {
-		if !eliminate(other, s, d2) {
-			return false
+func (n nstate) assign(s, d string) error {
+	// get all the values != d from square s
+	otherValues := strings.Replace(n[s], d, "", -1)
+	// eliminate each other value from the state at square s
+	for _, d2 := range otherValues {
+		if err := n.eliminate(s, string(d2)); err != nil {
+			return err
 		}
 	}
-	return true
+	return nil
 }
 
-func eliminate(values []string, s, d string) bool {
-	return true
+func (n nstate) has(sq, val string) bool {
+	return strings.Contains(n[sq], val)
+}
+
+func (n nstate) eliminate(s, d string) error {
+	// d is already eliminated
+	if !n.has(s, d) {
+		return nil
+	}
+
+	n[s] = strings.Replace(n[s], string(d), "", -1)
+	if len(n[s]) == 0 {
+		return ErrRemovedLastValue
+	} else if len(n[s]) == 1 {
+		d2 := n[s]
+		for _, p := range peers[s] {
+			if err := n.eliminate(p, d2); err != nil {
+				return err
+			}
+		}
+	}
+	// TODO: implement the rest
+	return nil
 }
